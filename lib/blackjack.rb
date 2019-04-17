4e8e086b0117cdaa74c7b44c32ee284664efe18f
feven <feventsegay6@gmail.com>
2019-04-17 21:55:11 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -44,6 +44,15 @@ end
 
 def hit?
   # code hit? here
+  prompt_user
+  user_input = get_user_input 
+  if user_input == "h"
+    card_total += deal_card 
+  elsif user_input == "s"
+    card_total 
+  else
+    invalid_command
+  end
 end
 
 def invalid_command
