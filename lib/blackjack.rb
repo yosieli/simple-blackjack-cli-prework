9b44d48cb757cef707e0a9b20e608f8c38f5eaec
feven <feventsegay6@gmail.com>
2019-04-17 21:15:08 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -28,9 +28,9 @@ def get_user_input
   
 end
 
-def end_game
+def end_game(card_total)
   # code #end_game here
-  puts "Sorry, you hit 27. Thanks for playing!"
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
